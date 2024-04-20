7518b544685de65d2ba1c843f0c4470bd52aaf5c
Sutanu-IT <sutanuchandra2002@gmail.com>
2024-04-20 19:30:30 +0530

    updated data_handler file and comment every line

--- data_handler/src/main/java/com/example/crawler/repository/CrawledDataRepository.java
+++ data_handler/src/main/java/com/example/crawler/repository/CrawledDataRepository.java
@@ -4,6 +4,8 @@ import com.example.crawler.model.CrawledData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Annotation to indicate that this interface is a repository
 @Repository
+// JpaRepository<CrawledData, Long> - JpaRepository for CrawledData entity with primary key of type Long
 public interface CrawledDataRepository extends JpaRepository<CrawledData, Long> {
 }
